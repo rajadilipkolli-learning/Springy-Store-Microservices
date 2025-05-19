88b91aa545e5086740c0d72418a14e2b5a2527de
Raja Kolli <rajadilipkolli@gmail.com>
2025-05-19 17:55:07 +0000

    refactor: migrate from SpringFox to Springdoc OpenAPI for API documentation

--- store-common/store-api/src/main/java/com/siriusxi/ms/store/api/composite/StoreEndpoint.java
+++ store-common/store-api/src/main/java/com/siriusxi/ms/store/api/composite/StoreEndpoint.java
@@ -1,10 +1,10 @@
 package com.siriusxi.ms.store.api.composite;
 
 import com.siriusxi.ms.store.api.composite.dto.ProductAggregate;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -20,7 +20,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  * @version v5.8
  * @since v3.0 codename Storm
  */
-@Api("REST API for Springy Store products information.")
+@Tag(name = "Store", description = "REST API for Springy Store products information.")
 @RequestMapping("store/api/v1")
 public interface StoreEndpoint extends StoreService {
 
@@ -33,25 +33,14 @@ public interface StoreEndpoint extends StoreService {
    * @return the composite product info, if found, else null.
    * @since v3.0 codename Storm.
    */
-  @ApiOperation(
-      value = "${api.product-composite.get-composite-product.description}",
-      notes = "${api.product-composite.get-composite-product.notes}")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            code = 400,
-            message = """
-                    Bad Request, invalid format of the request.
-                    See response message for more information.
-                    """),
-        @ApiResponse(code = 404, message = "Not found, the specified id does not exist."),
-        @ApiResponse(
-            code = 422,
-            message = """
-                    Unprocessable entity, input parameters caused the processing to fails.
-                    See response message for more information.
-                    """)
-      })
+  @Operation(
+      summary = "${api.product-composite.get-composite-product.description}",
+      description = "${api.product-composite.get-composite-product.notes}")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "400", description = "Bad Request, invalid format of the request. See response message for more information."),
+    @ApiResponse(responseCode = "404", description = "Not found, the specified id does not exist."),
+    @ApiResponse(responseCode = "422", description = "Unprocessable entity, input parameters caused the processing to fails. See response message for more information.")
+  })
   @GetMapping(value = "products/{id}",
           produces = APPLICATION_JSON_VALUE)
   @Override
@@ -70,24 +59,13 @@ public interface StoreEndpoint extends StoreService {
    * @since v3.0 codename Storm.
    * @return Nothing.
    */
-  @ApiOperation(
-      value = "${api.product-composite.create-composite-product.description}",
-      notes = "${api.product-composite.create-composite-product.notes}")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            code = 400,
-            message = """
-                Bad Request, invalid format of the request. 
-                See response message for more information.
-                """),
-        @ApiResponse(
-            code = 422,
-            message = """
-                Unprocessable entity, input parameters caused the processing to fail. 
-                See response message for more information.
-                """)
-      })
+  @Operation(
+      summary = "${api.product-composite.create-composite-product.description}",
+      description = "${api.product-composite.create-composite-product.notes}")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "400", description = "Bad Request, invalid format of the request. See response message for more information."),
+    @ApiResponse(responseCode = "422", description = "Unprocessable entity, input parameters caused the processing to fail. See response message for more information.")
+  })
   @PostMapping(
           value = "products",
           consumes = APPLICATION_JSON_VALUE)
@@ -103,25 +81,14 @@ public interface StoreEndpoint extends StoreService {
    * @since v3.0 codename Storm.
    * @return Nothing.
    */
-  @ApiOperation(
-      value = "${api.product-composite.delete-composite-product.description}",
-      notes = "${api.product-composite.delete-composite-product.notes}")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            code = 400,
-            message ="""
-                Bad Request, invalid format of the request. 
-                See response message for more information.
-                """),
-        @ApiResponse(
-            code = 422,
-            message ="""
-                Unprocessable entity, input parameters caused the processing to fail. 
-                See response message for more information.
-                """)
-      })
-  @DeleteMapping("products/{id}")
+  @Operation(
+      summary = "${api.product-composite.delete-composite-product.description}",
+      description = "${api.product-composite.delete-composite-product.notes}")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "400", description = "Bad Request, invalid format of the request. See response message for more information."),
+    @ApiResponse(responseCode = "422", description = "Unprocessable entity, input parameters caused the processing to fail. See response message for more information.")
+  })
+  @DeleteMapping(value = "products/{id}")
   @Override
   Mono<Void> deleteProduct(@PathVariable int id);
 }
