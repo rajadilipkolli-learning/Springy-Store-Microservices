4072fe873b8129201daa434367b51e95e74245af
Raja Kolli <rajadilipkolli@gmail.com>
2025-05-20 04:59:28 +0000

    upgrade to junit 5

--- store-services/store-service/src/test/java/com/siriusxi/ms/store/pcs/StoreServiceTestingSuite.java
+++ store-services/store-service/src/test/java/com/siriusxi/ms/store/pcs/StoreServiceTestingSuite.java
@@ -1,11 +1,10 @@
 package com.siriusxi.ms.store.pcs;
 
-import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
-import org.junit.runner.RunWith;
 
-//@RunWith(JUnitPlatform.class)
+@Suite
 @SuiteDisplayName("Store Service Testing Suite")
-@SelectClasses(value = {StoreServiceApplicationTests.class, MessagingTests.class})
+@SelectClasses({StoreServiceApplicationTests.class, MessagingTests.class})
 class StoreServiceTestingSuite {}
