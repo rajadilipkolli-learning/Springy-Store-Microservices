364554f72d3d7f51f6fecd97b8554374f0338a31
Raja Kolli <rajadilipkolli@gmail.com>
2025-05-20 05:14:54 +0000

    refactor: add testing dependencies and update StoreServiceTestingSuite class visibility

--- store-services/store-service/src/test/java/com/siriusxi/ms/store/pcs/StoreServiceTestingSuite.java
+++ store-services/store-service/src/test/java/com/siriusxi/ms/store/pcs/StoreServiceTestingSuite.java
@@ -1,10 +1,10 @@
 package com.siriusxi.ms.store.pcs;
 
 import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SuiteDisplayName("Store Service Testing Suite")
 @SelectClasses({StoreServiceApplicationTests.class, MessagingTests.class})
-class StoreServiceTestingSuite {}
+public class StoreServiceTestingSuite {}
